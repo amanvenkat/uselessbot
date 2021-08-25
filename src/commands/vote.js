579fe0045474d71f49fce3cf870f1c0d5f398cd2
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-08-25 20:20:46 +0530

    Update vote.js

--- src/commands/vote.js
+++ src/commands/vote.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.config = {
     name: 'vote', // Command Name
-    coinsInWallet: +=30000
+    user.coinsInWallet += 30000;
     description: 'allows you to vote.', // Description
     usage: 'h vote', // Usage
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
