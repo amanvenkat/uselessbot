d1875e93e565833c9ff1cdc07453c9524df0f722
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-08-25 22:35:24 +0530

    Update vote.js

--- src/commands/vote.js
+++ src/commands/vote.js
@@ -7,12 +7,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor("BLUE")
     .setDescription(`${i} **${usertag.user.username}** : Vote For Medu Wada Here : \n https://top.gg/bot/808350130352554004/vote`)
     message.channel.send(begembed).catch();
-    user.coinsInWallet +=30000;
-    
-    
 }
-
-//   https://top.gg/api/widget/upvotes/679710920334639115.svg?noavatar=true&leftcolor=1A191A&lefttextcolor=00CDCD&righttextcolor=1A191A&rightcolor=43b581
 
 module.exports.config = {
     name: 'vote', // Command Name
@@ -21,7 +16,7 @@ module.exports.config = {
     usage: 'h vote', // Usage
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
-    aliases: [], // Aliases 
-    bankSpace: 15, // Amount of bank space to give when command is used.
-    cooldown: 5 // Command Cooldown
+    aliases: [],  
+    bankSpace: 15, 
+    cooldown: 5 
 }
