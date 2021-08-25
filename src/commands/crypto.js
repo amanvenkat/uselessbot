fa15f12b699f2cd9850725e3a0124ddb164457fd
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-08-25 14:16:38 +0530

    Update crypto.js

--- src/commands/crypto.js
+++ src/commands/crypto.js
@@ -8,20 +8,22 @@ const BTC = '<:BTC:879991333123674132>'
 const ADA = '<:ADA:879991332817477672>'
 const ETH = '<:ETH:879991333161414656>'
 const MEDU = '<:MEDU:879991332708421634>'
+const BCH = '<:BCH:879991335606689832>'
+const GPU = '<:GPU:879993342753124403>'
 
 module.exports.run = async (bot, message, args) => {
   let user = await bot.fetchUser(message.author.id);
 const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
 
-const item = itemss.find(x => x.name.toLowerCase() === 'pickaxe');  
-let founditem = user.items.find(x => x.name.toLowerCase() === 'pickaxe');
+const item = itemss.find(x => x.name.toLowerCase() === 'GPU');  
+let founditem = user.items.find(x => x.name.toLowerCase() === 'GPU');
     let array = [];
-    array = user.items.filter(x => x.name !== 'pickaxe');
+    array = user.items.filter(x => x.name !== 'GPU');
     if (!founditem) {
               let use3embed = new MessageEmbed()
               .setColor("RED")
-              .setDescription(`${x} **${member.user.username}** : You don't own a \`PICKAXE\`, you need to buy one to use this command.`);
+              .setDescription(`${x} **${member.user.username}** : You don't own a \`GPU\`, you need to buy one to use this command.`);
               return message.channel.send(use3embed);
         //////return message.channel.send("you don't have this item");
     }
@@ -29,10 +31,10 @@ let founditem = user.items.find(x => x.name.toLowerCase() === 'pickaxe');
   
 
 const randomMessage = [
-  'd','d','d','d','d',
-  'r','r','r','r',
-  'g','g','g',
-  'a','a',
+  'd',
+  'r',
+  'g',
+  'a',
   'p',
   'missed','missed','missed','missed'
     ];
@@ -46,17 +48,17 @@ const randomMessage = [
         const Amount = Math.round(Math.random() * 1) + 1;
         const data = await bot.fetchUser(message.author.id);
         const Embeddiamond = new MessageEmbed()
-        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x Diamond Gem ${hd}.`)
+        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x BCH ${hd}.`)
         .setColor("GREEN")
         message.channel.send(Embeddiamond);
-        const findItem = data.items.find(i => i.name.toLowerCase() == 'diamond');
-        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'diamond');
+        const findItem = data.items.find(i => i.name.toLowerCase() == 'BCH');
+        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'BCH');
         if (findItem) {
-            userInv.push({ name: 'diamond', amount: (findItem.amount + Amount), description: `${hd} **Diamond Gem** \nsell the diamond to make money.` });
+            userInv.push({ name: 'BCH', amount: (findItem.amount + Amount), description: `${hd} **Bitcoin Cash** \nsell the Bitcoin Cash to make money.` });
             data.items = userInv;
             await data.save();
         } else {
-            userInv.push({ name: 'diamond', amount: Amount, description: `${hd} **Diamond Gem** \nsell the diamond to make money.` });
+            userInv.push({ name: 'BCH', amount: Amount, description: `${hd} **Bitcoin Cash** \nsell the Bitcoin Cash to make money.` });
             data.items = userInv;
             await data.save();
         }
@@ -64,17 +66,17 @@ const randomMessage = [
         const Amount = Math.round(Math.random() * 1) + 1;
         const data = await bot.fetchUser(message.author.id);
         const Embedruby = new MessageEmbed()
-        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x Ruby Gem ${hr}.`)
+        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x BTC ${hr}.`)
         .setColor("GREEN")
         message.channel.send(Embedruby);
-        const findItem = data.items.find(i => i.name.toLowerCase() == 'ruby');
-        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'ruby');
+        const findItem = data.items.find(i => i.name.toLowerCase() == 'BTC');
+        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'BTC');
         if (findItem) {
-            userInv.push({ name: 'ruby', amount: (findItem.amount + Amount), description: `${hr} **Ruby Gem** \nsell the ruby to make money.` });
+            userInv.push({ name: 'BTC', amount: (findItem.amount + Amount), description: `${hr} **Bitcoin** \nsell the bitcoin to make money.` });
             data.items = userInv;
             await data.save();
         } else {
-            userInv.push({ name: 'ruby', amount: Amount, description: `${hr} **Ruby Gem** \nsell the ruby to make money.` });
+            userInv.push({ name: 'BTC', amount: Amount, description: `${hr} **Bitcoin** \nsell the bitcoin to make money.` });
             data.items = userInv;
             await data.save();
         }
@@ -83,17 +85,17 @@ const randomMessage = [
         const Amount = Math.round(Math.random() * 1) + 1;
         const data = await bot.fetchUser(message.author.id);
         const Embedgade = new MessageEmbed()
-        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x Gade Gem ${hg}.`)
+        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x Cardano ${hg}.`)
         .setColor("GREEN")
         message.channel.send(Embedgade);
-        const findItem = data.items.find(i => i.name.toLowerCase() == 'gade');
-        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'gade');
+        const findItem = data.items.find(i => i.name.toLowerCase() == 'ADA');
+        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'ADA');
         if (findItem) {
-            userInv.push({ name: 'gade', amount: (findItem.amount + Amount), description: `${hg} **Gade Gem** \nsell the gade to make money.` });
+            userInv.push({ name: 'ADA', amount: (findItem.amount + Amount), description: `${hg} **Cardano** \nsell the cardano to make money.` });
             data.items = userInv;
             await data.save();
         } else {
-            userInv.push({ name: 'gade', amount: Amount, description: `${hg} **Gade Gem** \nsell the gade to make money.` });
+            userInv.push({ name: 'ADA', amount: Amount, description: `${hg} **Cardano** \nsell the cardano to make money.` });
             data.items = userInv;
             await data.save();
         }
@@ -102,17 +104,17 @@ const randomMessage = [
         const Amount = Math.round(Math.random() * 1) + 1;
         const data = await bot.fetchUser(message.author.id);
         const Embedveryrare = new MessageEmbed()
-        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x Amethyst Gem ${ha}.`)
+        .setDescription(`${tick} **${member.user.username}** : You went mining and came back with **${Amount}** x DOGE ${ha}.`)
         .setColor("GREEN")
         message.channel.send(Embedveryrare);
-        const findItem = data.items.find(i => i.name.toLowerCase() == 'amethyst');
-        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'amethyst');
+        const findItem = data.items.find(i => i.name.toLowerCase() == 'DOGE');
+        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'DOGE');
         if (findItem) {
-            userInv.push({ name: 'amethyst', amount: (findItem.amount + Amount), description: `${ha} **Amethyst Gem** \nsell the amethyst to make money.` });
+            userInv.push({ name: 'DOGE', amount: (findItem.amount + Amount), description: `${ha} **Doge coin** \nsell the DOGE to make money.` });
             data.items = userInv;
             await data.save();
         } else {
-            userInv.push({ name: 'amethyst', amount: Amount, description: `${ha} **Amethyst Gem** \nsell the amethyst to make money.` });
+            userInv.push({ name: 'DOGE', amount: Amount, description: `${ha} **Doge coin** \nsell the DOGE to make money.` });
             data.items = userInv;
             await data.save();
         }
@@ -121,17 +123,17 @@ const randomMessage = [
         const Amount = Math.round(Math.random() * 1) + 1;
         const data = await bot.fetchUser(message.author.id);
         const Embedled = new MessageEmbed()
-        .setDescription(`${tick} **${member.user.username}** : You went fishing and came back with **${Amount}** x Precious Gem ${hp}.`)
+        .setDescription(`${tick} **${member.user.username}** : You went fishing and came back with **${Amount}** x MEDUS ${hp}.`)
         .setColor("GREEN")
         message.channel.send(Embedled);
-        const findItem = data.items.find(i => i.name.toLowerCase() == 'precious');
-        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'precious');
+        const findItem = data.items.find(i => i.name.toLowerCase() == 'MEDU');
+        let userInv = data.items.filter(i => i.name.toLowerCase() !== 'MEDU');
         if (findItem) {
-            userInv.push({ name: 'precious', amount: (findItem.amount + Amount), description: `${hp} **Precious Gem** \nsell the precious to make money.` });
+            userInv.push({ name: 'MEDU', amount: (findItem.amount + Amount), description: `${hp} **Medus** \nsell the Medus to make money.` });
             data.items = userInv;
             await data.save();
         } else {
-            userInv.push({ name: 'precious', amount: Amount, description: `${hp} **Precious Gem** \nsell the precious to make money.` });
+            userInv.push({ name: 'MEDU', amount: Amount, description: `${hp} **Medus** \nsell the Medus to make money.` });
             data.items = userInv;
             await data.save();
         }
@@ -143,7 +145,7 @@ const randomMessage = [
         }
 }
 module.exports.config = {
-    name: 'mine', // Command Name
+    name: 'crypto', // Command Name
     description: 'use your pickaxe to find gems.', // Description
     usage: 'h mine', // Usage
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
