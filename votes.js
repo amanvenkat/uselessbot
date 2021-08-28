4c1bb051dcdaebf673d623498320027240f81e49
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-08-28 17:07:14 +0530

    m

--- votes.js
+++ votes.js
@@ -1,28 +1,25 @@
-const express = require('express')
-const Topgg = require('@top-gg/sdk')
-const fetch = require('node-fetch')
-const app = express()
-
-const webhook = new Topgg.Webhook('AmanBhai1970') //Secure Password (Change it for god's Sake)
-
-app.post('https://screeching-mini-cup.glitch.me/dblwebhook', webhook.listener(vote => { //ending url
-    console.log("User with id - " + vote.user + " Voted!")
-    let value = JSON.stringify({
-        embeds: [
-            {
-                title: "Another Vote!!",
-                description: `<@${vote.user}> (${vote.user}) Just Voted For \`The Bot\`!!`,
-                color: "8388736" //Hex -> Decimal
-            }
-        ]
-    })
-    fetch("https://discord.com/api/webhooks/881135128095641602/xAZE4GPyHF8g6hLcjb3l_z-QxwCbi1-hLKoDR9w2L4-JBUvbBqjV7Fa2r243ggFIeMpg", { //Your webhook here
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-        },
-        body: value
-    }).catch(e => console.log('Error occured while posting webhook : ' + e))
-}))
-app.listen(3000) //Port
-console.log("Your app is ready to log votes :D")
+const express = require('express'), {post, get} = require('superagent'), app = express();
+app.use(express.json()); 
+const db = require("quick.db");
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+})
+app.post('https://discord.com/api/webhooks/881135128095641602', async (req, res, data) => {
+    let auth = req.headers['authorization'];
+    if(!auth) return res.json({status: "Failed", message: `You didn't provide a 'Authorization' header!`});
+    if(auth !== "AmanBhai1970") return res.json({status: "Failed", message: `You didn't provide the correct authorization key!`});
+    if(!req.body) return res.json({status: "Failed", message: `You didn't provide any data!`});
+    if(!"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgwODM1MDEzMDM1MjU1NDAwNCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjMwMTUwNjExfQ.RcKlyLQeUsRpVr-QngWGtzal_biwNAou794trbgSwIc") {
+      console.log(`You didn't add your dbl api key in the .env file.. smh`)
+      return res.status(401).json({status: false, message: `Unauthorized`})
+    }
+let user = req.body.user,
+bot = req.body.bot;
+let logs = await db.fetch(`logs_${user}`)
+if (logs === null) logs = []
+data.logs.unshift(`[+2] - Voted Experience+ Bot in Top.gg.`)
+db.set(`logs_${user}`, logs) 
+db.add(`coins_${user}`, 2)
+     
+});
+app.listen(60002, () => console.log(`Listening on port 60002`));
