a10af9e4d57e539bdd83846b1d091fc4d8f6d2d4
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-09-13 01:26:13 +0530

    fuck it

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -3,13 +3,13 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(member => member.user.username === args.slice(0).join(' ') || member.user.username === args[0]) || message.member;
     const user = await bot.fetchUser(member.id);
-    const Embed = new MessageEmbed()
+    const embed = new MessageEmbed()
         .setTitle(`${member.user.username}'s Balance`)
         .setThumbnail(member.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
         .setDescription(`💳**Wallet**: ${user.coinsInWallet.toLocaleString()}\n🏦**Bank**: ${user.coinsInBank.toLocaleString()}/${user.bankSpace.toLocaleString()}\n🌐**Total Net Worth**: ${(user.coinsInWallet + user.coinsInBank).toLocaleString()}`)
-        .setFooter("\`[vote](https://top.gg/bot/808350130352554004/vote)\`")
+        .setFooter('Vote Here', 'https://top.gg/bot/808350130352554004')
         .setColor('RANDOM');
-    message.channel.send(Embed);
+    message.channel.send(embed);
 }
 
 module.exports.config = {
