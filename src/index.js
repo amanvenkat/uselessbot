3762e4668085d292940486029b8fe80fb5f0b1ff
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-09-29 15:46:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const MongoClient = require('./utils/MongoClient');
 const bot = new MongoClient({ ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] }, fetchAllMembers: false });
 const dbl = new DBL(process.env.WEBHOOK, bot);
 
-bot.login("ODg5NDc5MDkyOTkzNDE3MjU2.YUh19Q.9LHpx2CKbk0oR-az4AwsZBMRwbw");
+bot.login("ODg5NDc5MDkyOTkzNDE3MjU2.YUh19Q.KWNxxduYGMoYsWZb2nxPELUrX5M");
 
 bot.commands = new Collection();
 bot.aliases = new Collection();
