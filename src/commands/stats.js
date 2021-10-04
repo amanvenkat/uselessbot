e0de419f1e884d95aa79e41c46cb18926087c261
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-10-04 21:33:51 +0530

    Update stats.js

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     .setDescription(`${i} **Medu Wada** Live Stats :`)
     .addField(`**Servers :**`,` [** \`${bot.guilds.cache.size.toLocaleString()}\`** ]`)
     .addField(`**Users :**`,`[ **\`${message.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\`**] `)
-    .addField(`**Links** :`, "[\`Support Server\`](https://discord.gg/zyc9vDuWpD) | [\`Invite Me\`](https://discord.com/oauth2/authorize?client_id=887067763158450216&scope=bot&permissions=3221744704) | [\`Vote For Me\`](https://top.gg/bot/808350130352554004/vote) ")
+    .addField(`**Links** :`, "[\`Support Server\`](https://discord.gg/zyc9vDuWpD) | [\`Invite Me\`](https://discord.com/oauth2/authorize?client_id=887067763158450216&scope=bot&permissions=2148002896) | [\`Vote For Me\`](https://top.gg/bot/808350130352554004/vote) ")
     .setThumbnail(bot.user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
     .setColor("BLUE") //[\`s\`](https://docs.brandondev.xyz/)
     message.channel.send(Embed);
