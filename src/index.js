c4b5cbefb3e4be5e42a5203f91df391c49eca9a6
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-10-07 12:39:14 +0530

    square 1

--- src/index.js
+++ src/index.js
@@ -3,48 +3,7 @@ const { Collection } = require('discord.js');
 const DBL = require("dblapi.js");
 const MongoClient = require('./utils/MongoClient');
 const bot = new MongoClient({ ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS'] }, fetchAllMembers: false });
-const express = require('express');
-const app = express();
-const Topgg = require('@top-gg/sdk');
-const webhook = new Topgg.Webhook('test');
-const dbl = new DBL(process.env.TOPGGWEBHOOK, bot);
-
-app.get("/", (req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.write('<html><body><p>This is home Page.</p></body></html>');
-	res.end();
-})
-app.post("/votes/top.gg", webhook.middleware(), async (req, res) => {
-	res.status(200).end()
-	const vote = req.vote;
-	client.channels.cache.get('895343074308411423').send(vote.user)
-	let result = await db.fetch(vote.user);
-    const usertag = message.member;
-    const random = 50000;
-    const supportrandom = 50000;
- 
-    if (vote.guild === '892416701851910144') {
-        bot.giveCoins(vote.user, random + supportrandom);
-	} else {
-		if (vote.isWeekend) {
-		bot.giveCoins(vote.user, random + supportrandom);	
-		} else {
-            bot.giveCoins(vote.user, random);	
-		
-
-		}
-		result.items.box = parseInt(newbox)
-		db.set(vote.user, 'items', result.items);
-
-
-
-	}
-
-
-})
-
-
-app.listen(5000);
+const dbl = new DBL(process.env.WEBHOOK, bot);
 
 bot.login(process.env.TOKEN);
 
