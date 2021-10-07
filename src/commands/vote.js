a6fb330549d24278bc224f428bac448c455354f5
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-10-07 13:52:31 +0530

    voteRewards

--- src/commands/vote.js
+++ src/commands/vote.js
@@ -1,4 +1,3 @@
-const x = '<:crossed:894509079031066644>'
 const { MessageEmbed } = require("discord.js");
 const itemss = require('../utils/items');
 const i = 'ℹ'
@@ -6,12 +5,18 @@ const btc = '<:BTC:879991333123674132>'
 module.exports.run = async (bot, message, args) => {
     const usertag = message.member;
     const random = 50000;
+    const randoms = 75000;
     let begembed = new MessageEmbed()
-    .setAuthor(`VOTE THE BOT TO GET 50000 COINS`)
+    .setAuthor(`**VOTE THE BOT TO GET 50000 COINS**`)
     .setColor("BLUE")
-    .setDescription(`${i} **${usertag.user.username}** : [Vote Here](https://discordbotlist.com/bots/medu-wada-0478/upvote)`)
+    .setDescription(`${i} **${usertag.user.username}** : [Vote Here](https://discordbotlist.com/bots/medu-wada/upvote)\n**Get 25000 extra coins on voting next time from the** [Support Server](https://discord.gg/zyc9vDuWpD)`)
     message.channel.send(begembed).catch();
+
+    if (message.guild.id === '892416701851910144') {
+    await bot.giveCoins(message.author.id, randoms);
+    } else {
     await bot.giveCoins(message.author.id, random);
+    }
 }
 
 module.exports.config = {
