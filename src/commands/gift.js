8fbfea2e737b18995d980da8f2070e19671896f6
amanvenkat <78382006+amanvenkat@users.noreply.github.com>
2021-10-08 17:58:44 +0530

    gift

--- src/commands/gift.js
+++ src/commands/gift.js
@@ -1,7 +1,7 @@
-const x = '<:crossed:894509079031066644>'
-const tick = '<:check:894509078586470401>'
 const itemss = require('../utils/items');
 const i = 'ℹ'
+const x = '<:crossed:894509079031066644>'
+const tick = '<:check:894509078586470401>'
 const { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
@@ -94,6 +94,6 @@ module.exports.config = {
     botPerms: [], // Bot permissions needed to run command. Leave empty if nothing.
     userPerms: [], // User permissions needed to run command. Leave empty if nothing.
     aliases: [], // Aliases 
-    bankSpace: 15, // Amount of bank space to give when command is used.
+    bankSpace: 7, // Amount of bank space to give when command is used.
     cooldown: 5 // Command Cooldown
 }
